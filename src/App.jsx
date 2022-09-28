8e7438ce0cb5b20dca9f796dcff6ca95507d8cad
rjaybernardo <raphaeljaybernardo@gmail.com>
2022-09-29 04:21:14 +0800

    About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from './components/Navbar'
 import SocialLinks from './components/SocialLinks'
 import Home from './components/Home/index'
+import About from './components/About/index'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
       <SocialLinks />
     </div>
   )
