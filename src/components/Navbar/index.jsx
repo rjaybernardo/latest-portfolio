33c6d082537cc03152f059cdb6d57e2c9c641102
rjaybernardo <raphaeljaybernardo@gmail.com>
2022-09-29 03:57:13 +0800

    mobile nav

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -13,10 +13,42 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav)
 
   return (
-    <div className='fixed flex h-[80px] w-full items-center justify-between bg-[#0a192f] px-4 font-bold text-gray-300'>
+    <div className='fixed flex h-[80px] w-full items-center justify-between bg-[#0a192f] px-4 font-bold capitalize text-gray-300'>
       <div>
         <img src={Logo} alt='Logo' style={{ width: '100px' }} />
       </div>
+      {/* menu */}
+      <ul className='hidden md:flex'>
+        {links.map(({ id, link }) => (
+          <li key={id}>
+            <Link to={link} smooth={true} duration={500}>
+              {link}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className='z-10 text-3xl md:hidden'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 flex h-screen w-full flex-col items-center justify-center bg-[#0a192f]'
+        }
+      >
+        {links.map(({ id, link }) => (
+          <li key={id} className='py-6 text-4xl'>
+            <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+              {link}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
