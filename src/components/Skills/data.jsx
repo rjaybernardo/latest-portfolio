078292e11a9ff27fa3b362b73369d0862e4556a7
rjaybernardo <raphaeljaybernardo@gmail.com>
2023-11-09 10:26:32 +0800

    skills updated

--- src/components/Skills/data.jsx
+++ src/components/Skills/data.jsx
@@ -4,57 +4,57 @@ import Javascript from '/images/javascript.png'
 import React from '/images/react.png'
 import Github from '/images/github.png'
 import Node from '/images/node.png'
-import Mongo from '/images/mongo.png'
-import Firebase from '/images/firebase.png'
+import Webflow from '/images/webflow.png'
+import Shopify from '/images/shopify.png'
 
 const tech = [
   {
     id: 1,
+    title: 'Shopify',
+    src: Shopify,
+    style: 'shadow-green-400',
+  },
+  {
+    id: 2,
+    title: 'Webflow',
+    src: Webflow,
+    style: 'shadow-green-600',
+  },
+  {
+    id: 3,
     title: 'HTML',
     src: Html,
     style: 'shadow-orange-500',
   },
   {
-    id: 2,
+    id: 4,
     title: 'CSS',
     src: Css,
     style: 'shadow-blue-500',
   },
   {
-    id: 3,
+    id: 5,
     title: 'Javascript',
     src: Javascript,
     style: 'shadow-yellow-500',
   },
   {
-    id: 4,
+    id: 6,
     title: 'React',
     src: React,
     style: 'shadow-blue-600',
   },
   {
-    id: 5,
+    id: 7,
     title: 'Github',
     src: Github,
     style: 'shadow-white',
   },
   {
-    id: 6,
+    id: 8,
     title: 'NodeJS',
     src: Node,
     style: 'shadow-green-300',
-  },
-  {
-    id: 7,
-    title: 'MongoDB',
-    src: Mongo,
-    style: 'shadow-green-600',
-  },
-  {
-    id: 8,
-    title: 'Firebase',
-    src: Firebase,
-    style: 'shadow-yellow-600',
   },
 ]
 
